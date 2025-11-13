44ee6ce43055aa9ff42e642559f6f9d89e654ed6
João Daniel <mr.joao200022@gmail.com>
2025-11-12 21:41:18 -0300

    Meu primeiro commit via terminal no VScode!

---
+++ clientes/clientes.py
@@ -0,0 +1,59 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Loads environment variables of .env file
+load_dotenv()
+
+# Costumer registration funcion
+def costumer_record(connection):
+    # cursor
+    with connection.cursor() as cursor:
+
+        # Variables
+        while True:
+            ddd = input("DDD: ")
+            if ddd.isdigit() and len(ddd) == 2:
+                break
+        while True:
+            number = input("Número: ")
+            if number.isdigit() and 8 <= len(number) <= 9:
+                break
+
+        phone = ddd + number 
+
+        name = input("Nome: ")
+
+        values = (name, phone)
+
+        # SQL commands
+        sql = "INSERT INTO clientes (name, phone) VALUES (%s, %s)"
+
+
+        cursor.execute(sql, values)
+        connection.commit()
+
+        print("Cliente cadastrado com sucesso!")
+
+# Main
+ 
+try:
+    # Server information and connection
+    with psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        sslmode=os.getenv("DB_SSLMODE") 
+    )as connection:
+    print("Connection seccessfully!")
+
+        costumer_record(connection)
+
+
+except psycopg2.OperationalError as e:
+    print("Erro na conexão com o banco de dados", e)
+
+except psycopg2.Error as e:
+    print("Erro ao executar o comando SQL!", e)
